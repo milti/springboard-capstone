563c9589b3bfda09fcd2f5764d617f4edac73c34
milti <milti@mac.com>
2017-07-19 20:32:34 -0500

    committing steps forward

---
+++ 19july2017.R
@@ -0,0 +1,70 @@
+knitr::opts_chunk$set(echo = TRUE)
+libary(RNeo4j)
+library(twitteR)
+requestURL <- "https://api.twitter.com/oauth/request_token"
+accessURL <- "https://api.twitter.com/oauth/access_token"
+authURL <- "https://api.twitter.com/oauth/authorize"
+consumer_key <- '8D843UpqjLV3kOhckizpHXKd4'
+consumer_secret <- 'EXRkgexU1KLCZFeVEfrbgfHYvRFdqCVCxmYaJw06ze6nEQZInj'
+access_token <- '1240280636-VNnxhIGWUP5UDWftvU8Ts7tb2ypdjAhFKBIhjBY'
+access_secret <- 'I2lRPYXhGgdsUJhqbxzoC2CSuov7ClAlDkjuabXVLzBVF'
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+source('~/R/springboard twitteR/19july2017.R')
+source('~/R/springboard twitteR/19july2017.R')
+sessionInfo()
+source('~/R/19july2017.R')
+source('~/R/19july2017.R')
+sessionInfo()
+library(reticulate)
+library(XML)
+path_to_python <- "/usr/local/bin/python3.6"
+use_python(path_to_python)
+dbot=import("debot")
+db = dbot$DeBot('DZgJyWsdPQlbU5ManszuakZRSnldBWn5LuE9hXdX')#This is my API key
+bots=htmlParse(db$get_related_bots('election2016'))#Change 'election2016' to any other topic of your interest
+#Now parse the XML data into a dataframe
+botlist <- xmlToList(bots)
+library(twitteR)
+requestURL <- "https://api.twitter.com/oauth/request_token"
+accessURL <- "https://api.twitter.com/oauth/access_token"
+authURL <- "https://api.twitter.com/oauth/authorize"
+consumer_key <- '8D843UpqjLV3kOhckizpHXKd4'
+consumer_secret <- 'EXRkgexU1KLCZFeVEfrbgfHYvRFdqCVCxmYaJw06ze6nEQZInj'
+access_token <- '1240280636-VNnxhIGWUP5UDWftvU8Ts7tb2ypdjAhFKBIhjBY'
+access_secret <- 'I2lRPYXhGgdsUJhqbxzoC2CSuov7ClAlDkjuabXVLzBVF'
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+map_data(botlist)
+library(maps)
+library(tidyverse)
+install.packages("listviewer")
+library(listviewer)
+lapply(botList, function(x) x[[1]])
+sapply(botlist, `[[`, 1)
+lapply(botlist, `[[`, 1)
+flatbot <- flatten(botlist)
+flatterbot <- flatten(flatbot)
+remove(flatbot)
+flatbot <- flatten(flatterbot)
+flatbot %>% map_chr("screen_name")
+botbod <- flatbot %>% map_chr("screen_name")
+lapply(flatbot, `[[`, 2)
+lapply(flatbot, `[[`, 1)
+flatbot
+lapply(flatbot, `[`, 2)
+unname(sapply(flatbot, `[`, 2))
+newBotlist <- unname(sapply(flatbot, `[`, 2))
+as.data.frame(newBotlist)
+View(newBotlist)
+botAcctlist <- lapply(newBotlist,getUser)
+?twitteR
+library(twitteR)
+botAcctlist <- lapply(newBotlist,getUser)
+botAcctlist <- lapply(newBotlist,lookupUsers)
+botAcctlist <- sapply(newBotlist,lookupUsers)
+botAcctlist <- vapply(newBotlist,lookupUsers)
+botAcctlist <- apply(newBotlist,lookupUsers)
+botAcctlist <- lapply(newBotlist,getUser())
+botAcctlist <- lapply(newBotlist[],getUser())
+savehistory("~/R/springboard\ twitteR/19july2017.R")
+savehistory("~/R/github/springboard\ capstone/19july2017.R")
+savehistory("~/R/github/springboard\ capstone/19july2017.R")
